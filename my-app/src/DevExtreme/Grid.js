e97ba1f5ee952675d29ddb8319d63d3b841f722a
ktvee <89483452+ktvee@users.noreply.github.com>
2021-08-25 14:39:06 -0400

    Removed excess API operations
    Adjusted the code to remove extra DevExtreme components not needed at the moment, and modified the API call to confirm if data is returned

--- my-app/src/DevExtreme/Grid.js
+++ my-app/src/DevExtreme/Grid.js
@@ -18,139 +18,33 @@ class Grid extends React.Component {
     super(props);
     this.state = {
       fbData: null,
-      ordersData: new CustomStore({
-        key: 'OrderID',
-        load: () => this.sendRequest(`${URL}`, 'GET'),
-      }),
-      requests: [],
-      refreshMode: 'reshape'
     };
-    this.clearRequests = this.clearRequests.bind(this);
-    this.handleRefreshModeChange = this.handleRefreshModeChange.bind(this);
-
-  var body = 'Reading JS SDK documentation';
-  FB.api('/me/feed', 'post', { message: body }, function(response) {
-    if (!response || response.error) {
-      console.log('Error occured');
-    } else {
-      console.log('Post ID: ' + response.id);
+    // calling from the api
+  FB.api(
+    '/170107151801959/feed', 'GET', {}, function(response) {
+      console.log('GET response: ', response.data)
+        // Insert your code here
+        this.setState({
+          'fbData': response.data
+        })
     }
-  })
+  );
+
 }
 
-  sendRequest(url, method, data) {
-    method = method || 'GET';
-    data = data || {};
-
-    this.logRequest(method, url, data);
-
-    if(method === 'GET') {
-      return fetch(url, {
-        method: method,
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-          'Access-Control-Allow-Origin': '*'
-        }
-      }).then(result => result.json().then(json => {
-        if(result.ok) return json.data;
-        throw json.Message;
-      }));
-    }
-
-    const params = Object.keys(data).map((key) => {
-      return `${encodeURIComponent(key) }=${ encodeURIComponent(data[key])}`;
-    }).join('&');
-
-    return fetch(url, {
-      method: method,
-      body: params,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-      },
-      credentials: 'include'
-    }).then(result => {
-      if(result.ok) {
-        return result.text().then(text => text && JSON.parse(text));
-      } else {
-        return result.json().then(json => {
-          throw json.Message;
-        });
-      }
-    });
-  }
-
-  logRequest(method, url, data) {
-    const args = Object.keys(data || {}).map(function(key) {
-      return `${key }=${ data[key]}`;
-    }).join(' ');
-
-    const time = formatDate(new Date(), 'HH:mm:ss');
-    const request = [time, method, url.slice(URL.length), args].join(' ');
-
-    this.setState((state) => {
-      return { requests: [request].concat(state.requests) };
-    });
-  }
-
-  clearRequests() {
-    this.setState({ requests: [] });
-  }
-
-  handleRefreshModeChange(e) {
-    this.setState({ refreshMode: e.value });
-  }
-
   render() {
-    const { refreshMode, ordersData } = this.state;
     return (
       <React.Fragment>
         <DataGrid
           id="grid"
           showBorders={true}
-          dataSource={ordersData}
+          dataSource={this.state.fbData}
           repaintChangesOnly={true}
         >
-          <Editing
-            refreshMode={refreshMode}
-            mode="cell"
-            allowAdding={true}
-            allowDeleting={true}
-            allowUpdating={true}
-          />
-
           <Scrolling
             mode="virtual"
           />
-
-          <Lookup dataSource={ordersData} valueExpr="Value" displayExpr="Text" />
-
-          <Summary>
-            {/* <TotalItem column="CustomerID" summaryType="count" />
-            <TotalItem column="Freight" summaryType="sum" valueFormat="#0.00" /> */}
-          </Summary>
-
         </DataGrid>
-        <div className="options">
-          <div className="caption">Options</div>
-          <div className="option">
-            <span>Refresh Mode: </span>
-            <SelectBox
-              value={refreshMode}
-              items={REFRESH_MODES}
-              onValueChanged={this.handleRefreshModeChange}
-            />
-          </div>
-          <div id="requests">
-            <div>
-              <div className="caption">Network Requests</div>
-              <Button id="clear" text="Clear" onClick={this.clearRequests} />
-            </div>
-            <ul>
-              {this.state.requests.map((request, index) => <li key={index}>{request}</li>)}
-            </ul>
-          </div>
-        </div>
       </React.Fragment>
     );
   }
